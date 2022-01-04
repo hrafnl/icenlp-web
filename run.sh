8c28d2bb6506f5bb80e9ff338a99d6331307c8f4
GlacierSG <jokullsnr@gmail.com>
2022-01-04 14:23:50 +0000

    fixed html error

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-docker container stop icenlp
-docker container rm icenlp
-docker build . -t icenlp:example
-docker run -d --name=icenlp -p 8080:8080 icenlp:example
+docker container stop glaciersg/icenlp:v1.0
+docker container rm glaciersg/icenlp:v1.0
+docker build . -t glaciersg/icenlp:v1.0
+docker run -d --name=icenlp -p 8080:8080 glaciersg/icenlp:v1.0
